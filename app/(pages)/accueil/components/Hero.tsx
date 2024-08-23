48d1a7bb0d9b8f147db3d0f574bc256e8dc42c86
Alexandre <alexandre.hernandez@yahoo.com>
2024-08-23 15:33:37 +0200

    reactIcon

--- app/(pages)/accueil/components/Hero.tsx
+++ app/(pages)/accueil/components/Hero.tsx
@@ -1,27 +1,31 @@
+// Fichier principal pour le composant Hero
+
 import ButtonLink from "@/app/components/Button/ButtonLink";
 import Picture from "@/app/components/Picture";
 import { Box, Typography } from "@mui/material";
-import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import { IoArrowForwardCircleOutline } from "react-icons/io5"; // Assurez-vous que ce module est installé
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import SwipeDownloadButton from "./SwipeDownloadButton";
+
+// Fonction principale pour le composant Hero
 export default function Hero() {
   return (
     <Box component="section" display="flex" flexDirection="column" alignItems="start" justifyContent="center" gap="1.6rem" >
-    <Picture />
-    <Typography variant="h1" >
-      <Box color="var(--mui-palette-primary-main)" component="span" >
-        Bonjour!
+      <Picture />
+      <Typography variant="h1" >
+        <Box color="var(--mui-palette-primary-main)" component="span" >
+          Bonjour!
+        </Box>
+        Je suis Alexandre Hernandez
+      </Typography>
+      <Typography variant="h4" >
+        Bienvenue sur mon portfolio de développeur web basé à Tarbes, France.
+      </Typography>
+      <Box width="100%" display="flex" gap="1.6rem" >
+        <ButtonLink label="Mes Projets" variant="contained" icon={<IoArrowForwardCircleOutline />} to="/projets" />
+        <ButtonLink label="Contact" variant="outlined" icon={<EmailRoundedIcon />} to="/contact" />
       </Box>
-      Je suis Alexandre Hernandez
-    </Typography>
-    <Typography variant="h4" >
-      Bienvenue sur mon portfolio de développeur web basé à Tarbes, France.
-    </Typography>
-    <Box width="100%" display="flex" gap="1.6rem" >
-      <ButtonLink label="Mes Projets" variant="contained" icon={<IoArrowForwardCircleOutline />} to="/projets" />
-      <ButtonLink label="Contact" variant="outlined" icon={<EmailRoundedIcon />} to="/contact" />
+      <SwipeDownloadButton />
     </Box>
-    <SwipeDownloadButton />
-  </Box>
   )
 }
