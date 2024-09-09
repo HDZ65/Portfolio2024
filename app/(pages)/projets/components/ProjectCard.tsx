9c77b5d12c46111a61b9ebacb8eef218520c94d8
Alexandre <alexandre.hernandez@yahoo.com>
2024-09-09 13:38:57 +0200

    shadow

--- app/(pages)/projets/components/ProjectCard.tsx
+++ app/(pages)/projets/components/ProjectCard.tsx
@@ -4,12 +4,10 @@
 import { Typography, Box } from '@mui/material';
 import { Project, projects } from '../data/projets';
 import Image from 'next/image';
-import { useState } from 'react';
 import { useProjectContext } from '../context/useProjectContext';
 
 // Fonction pour afficher une carte de projet
 export default function ProjectCard() {
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
     const { setOpenProject, openProject } = useProjectContext();
 
     return (
@@ -18,8 +16,6 @@ export default function ProjectCard() {
                 <Box 
                     display={{xs: 'none', md: 'block'}}
                     onClick={() => setOpenProject(project)}
-                    onMouseEnter={() => setHoveredIndex(index)}
-                    onMouseLeave={() => setHoveredIndex(null)}
                     aria-label={`Voir les détails du projet ${project.title}`} 
                     key={index} 
                     sx={{ 
@@ -29,7 +25,6 @@ export default function ProjectCard() {
                         boxShadow: openProject?.slug === project.slug ? '0 0 20px 0 var(--mui-palette-primary-main)' : 'none',
                         borderRadius: '30px',
                         overflow: 'hidden',
-
                     }}
                     className='group'
                 >
@@ -37,10 +32,15 @@ export default function ProjectCard() {
                         src={project.image}
                         alt={project.title}
                         fill
-
-                        className='group-hover:brightness-[0.5] object-cover object-top transition-all duration-300'
-
-
+                        className='object-cover object-top transition-all duration-300 group-hover:brightness-75 group-hover:blur-xs'
+                    />
+                    <Box
+                        position='absolute'
+                        top={0}
+                        left={0}
+                        right={0}
+                        bottom={0}
+                        className='transition-all duration-300 backdrop-blur-sm bg-black/30 opacity-0 group-hover:opacity-100'
                     />
                     <Box
                         position='absolute'
@@ -48,11 +48,11 @@ export default function ProjectCard() {
                         left='50%'
                         sx={{
                             transform: "translate(-50%, -50%)",
-                            transition: "all 0.3s ease-in-out",
-                            opacity: hoveredIndex === index ? 1 : 0,
                             textAlign: 'center', 
-                            width: '100%'  
+                            width: '100%',
+                            zIndex: 1,
                         }}
+                        className='transition-all duration-300 opacity-0 group-hover:opacity-100'
                     >
                         <Typography variant='h2'>{project.title}</Typography>
                         <Typography sx={{ color: "var(--mui-palette-primary-main)", textDecoration: 'underline' }}>
