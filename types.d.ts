d2d7af5144059e59a500e683b9323df4f50ec6fb
HDZ65 <alexandre.hernandez@yahoo.com>
2025-04-21 16:46:11 +0200

    d

---
+++ types.d.ts
@@ -0,0 +1,5 @@
+declare module '@mui/icons-material/WorkRounded' {
+    import { SvgIconProps } from '@mui/material/SvgIcon';
+    const WorkRounded: React.ComponentType<SvgIconProps>;
+    export default WorkRounded;
+} 
